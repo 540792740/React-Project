8cda114547d72abcf848bb5d9176be4496c7b193
peter <peterwangjiawei@gmail.com>
2020-05-14 21:11:39 -0500

    May 14 react 123456

--- src/React/react-demo01/src/component/News.js
+++ src/React/react-demo01/src/component/News.js
@@ -10,9 +10,10 @@ class News extends Component {
     }
     render() {
         return (
-            <div>
+            <div className='news'>
                 <h3>{this.state.msg}</h3>
                 <img src={logo}/>
+                <img src="https://dss1.bdstatic.com/6OF1bjeh1BF3odCf/it/u=1790140221,4180337232&fm=173&app=49&f=JPEG?w=218&h=146&s=09F8ED175E236401164B51640300E068"/>
             </div>
         );
     }
