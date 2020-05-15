b8130d5bb43a98ac5371109d19db567d0e4b9607
peter <peterwangjiawei@gmail.com>
2020-05-14 22:33:10 -0500

    May 14 react 912

--- src/React/react-demo01/src/component/Home.js
+++ src/React/react-demo01/src/component/Home.js
@@ -29,6 +29,22 @@ class Home extends Component {
         alert(this.state.msg)
     }
 
+    getData3 =() =>{
+        alert(this.state.msg)
+    }
+
+    setData = () =>{
+        this.setState({
+            msg:"This is a new Component, set new state for the component."
+        })
+    }
+
+    setName=(msg) =>{
+        this.setState({
+            name:msg
+        })
+    }
+
     render() {
         return (
             <div>
@@ -41,10 +57,18 @@ class Home extends Component {
                 {/*<div style={this.state.style}>This is a blue style font</div>*/}
 
                 {/*Class 4*/}
+                <h3>{this.state.msg}</h3>
+                <h3>{this.state.name}</h3>
                 <button onClick={this.run}>Run</button>
                 <button onClick={this.getData.bind(this)}>Type1: getData</button>
                 <button onClick={this.getData2}>Type2: getData</button>
+                <button onClick={this.getData3}>Type3: getData</button>
+                <button onClick={()=>this.getData()}>Type4: getData</button>
+                <hr/>
+                <button onClick={this.setData}>set state</button>
 
+                {/*A way to set parameter*/}
+                <button onClick={this.setName.bind(this, ("Cris Peter"))}>set state</button>
 
             </div>
         );
