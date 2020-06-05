01839cf86e41ccafff1961c23514b396a1a4bab2
peter <peterwangjiawei@gmail.com>
2020-06-04 22:43:28 -0500

    Jun 4 Node 12

--- src/Node.md
+++ src/Node.md
@@ -1,10 +1,20 @@
-####1. HTTP module
+####1. HTTP module, url Module
 ```
     var http = require('http');
+    var url = require('url');
+
     http.createServer(func(req, res){
+        var query = url.parse(req.url, true);        
+
         res.writeHead();
         res.write();
         res.end();    
 }).listen(8888)
-
 ```
+
+
+####2. Supervisor
+add a listener
+`npm -g install supervisor`
+
+run server in terminal: `supervisor app.js`
--- src/NodeJs/02_HttpModule.js
+++ src/NodeJs/02_HttpModule.js
@@ -1,10 +1,20 @@
 var http = require('http');
+var url = require('url');
 http.createServer(function (request, response) {
+    console.log(request.url)
+    if(request.url !== '/favicon.ico'){
 
+        // parseQueryString, make result become a object
+        var result = url.parse(request.url, true);
+        console.log(result.query.aid)
+
+    }
     response.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
     response.write("test once <br>")
     response.write("test  <br>")
+    response.write("New Test  <br>")
     response.end("哈哈哈哈，我买了一个iPhone" + (1+2+3) + "s");
 }).listen(8888);
 
 console.log('Server running at http://127.0.0.1:8888/');
+
