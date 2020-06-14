15285f1be54bf41c79a6cd6d61a493b30dd797b3
peter <peterwangjiawei@gmail.com>
2020-06-14 08:43:18 -0500

    Jun 14 Node 123456

---
+++ src/NodeJs/09_NonBlocking_IO/04_FetchData.js
@@ -0,0 +1,20 @@
+var events = require('events');
+var fs = require('fs');
+
+
+var EventEmitter = new events.EventEmitter();
+
+function getMime() {
+    fs.readFile('../mime.json', function (err, data) {
+        EventEmitter.emit('to_parent',data.toString());
+
+    })
+}
+getMime();
+
+EventEmitter.on('to_parent', function(data){
+    console.log("Received")
+    console.log(data);
+    EventEmitter.emit('to_Mime','Data In Mime');
+
+});
