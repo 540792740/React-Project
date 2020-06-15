d10825856d24698784a6505811fc9d8195e6b599
peter <peterwangjiawei@gmail.com>
2020-06-14 20:04:53 -0500

    Jun 14 Node 91

--- src/NodeJs/10_NodeJs get Post/00services2.js
+++ src/NodeJs/10_NodeJs get Post/00services2.js
@@ -5,15 +5,27 @@ var router = require('./Router')
 var ejs = require('ejs');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {"Content-Type": "rext/html; charset='utf-8'"});
+    res.writeHead(200, {"Content-Type": "text/html; charset='utf-8'"});
     var pathname = url.parse(req.url).pathname;
     if(pathname === '/login'){
-        ejs.renderFile('./Views/login.ejs', {}, function (err, data) {
+        var data = 'Backend Data'
+        ejs.renderFile('./Views/login.ejs', {
+        msg:data,
+        list:['111','222']
+        }, function (err, data) {
             res.end(data);
         })
     }
     else{
-        res.end('index')
+        var data = 'Register Data';
+        var h = "<h2>This is a h2 tag</h2>"
+        ejs.renderFile('./Views/register.ejs', {
+            msg:data,
+            list:['111','222'],
+            h:h
+        }, function (err, data) {
+            res.end(data);
+        })
     }
 }).listen(8888);
 
