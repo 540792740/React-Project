0e0dbe81fcf0718d2aaabfc860386be4bb07f0d3
peter <peterwangjiawei@gmail.com>
2020-06-21 17:45:21 -0500

    Jun 19 1225678

--- src/React/todo-list-practive/src/App.js
+++ src/React/todo-list-practive/src/App.js
@@ -21,35 +21,58 @@ class App extends Component {
                 <input type="text"
                        value={this.state.newItem}
                        className='inputText'
-                       onChange={e => this.updateItem('newItem', e.target.value)}
+                       onChange={e => this.updateItem(e.target.value)}
                        placeholder='Add Item'/>
-                <button className='btn'
+                <button className='btn btn-add'
                         onClick={() => this.addItems()}>+</button>
                 <ul>
                     {this.state.list.map(item =>{return (
-                        <li >
-                            <button onClick={() => this.deleteItem()}>X</button>
+                        <li className='item' key={item.id}>
+                            {item.value}
+                            <button className="btn btn-delete"
+                                    onClick={() => this.deleteItem(item.id)}>
+                                x
+                            </button>
                         </li>
                     )})}
                 </ul>
-
-
             </div>
         </div>
     );
   }
-
-    updateItem() {
-
-    }
+    updateItem( value) {
+        this.setState({
+            ['newItem']: value
+        });
+  }
 
     addItems() {
+        const newItem = {
+            id:  Math.random(),
+            value : this.state.newItem
+        }
+        // copy list
+        var list = [...this.state.list];
 
+        list.push(newItem)
+
+        // Renew State
+        this.setState({
+            newItem:'',
+            list,
+        })
     }
 
-    deleteItem(){
+    deleteItem(item){
+        // copy list
+        var list = [...this.state.list];
 
-    }
+        var updateList = list.filter(temp => temp.id !== item)
+
+        this.setState({
+            list:updateList,
+        })
+  }
 }
 
 export default App;
