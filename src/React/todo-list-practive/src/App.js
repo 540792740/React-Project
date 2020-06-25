a62e2c0d7029f11d7df428430209b76538e9e4f9
peter <peterwangjiawei@gmail.com>
2020-06-25 16:38:42 -0500

    Jun 25 react 12

--- src/React/todo-list-practive/src/App.js
+++ src/React/todo-list-practive/src/App.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
 import './App.css';
+import Person from "./Person/Person";
 
 class App extends Component {
     constructor(props) {
         super(props);
     }
+    state={
+        persons:[
+            {id:'1', name:'Peter', age:12},
+            {id:'2', name:'Cris', age:22},
+            {id:'3', name:'Jack', age:32},
+        ]
+    }
+
     render() {
         return (
-            <div>
+            <div className='App'>
+                <p>React</p>
+                <Person name={this.state.persons[0].name}
+                        id={this.state.persons[0].id}
+                        age={this.state.persons[0].age}
 
+                />
             </div>
         );
     }
---
+++ src/React/todo-list-practive/src/Person/Person.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Person =()=>{
+    return(
+        <div className='Person'>
+            <p>I am {prop.name}, I am 10 years old</p>
+        </div>
+    )
+}
+export default Person;
