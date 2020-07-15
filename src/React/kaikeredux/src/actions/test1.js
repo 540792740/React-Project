4db82af53a17a65dac1d0a1b708e4ace126407bb
peter <peterwangjiawei@gmail.com>
2020-07-14 21:40:03 -0500

    Jul 14 react 1234567891011

--- src/React/kaikeredux/src/actions/test1.js
+++ src/React/kaikeredux/src/actions/test1.js
@@ -8,9 +8,11 @@ function checkAdd(arr){
         let row = parseInt(arr[i * 3 ]) +
                     parseInt(arr[i * 3  + 1]) +
                     parseInt(arr[i * 3  + 2])
+
         let col = parseInt(arr[0 + i]) +
                     parseInt(arr[3 + i]) +
                     parseInt(arr[6 + i])
+
         if(row !== 15 || col !== 15) return false
     }
     let leftSlope = parseInt(arr[0]) +
@@ -23,30 +25,32 @@ function checkAdd(arr){
     return true
 }
 
- function fullpermutate(str) {
-     var result = [];
-     if (str.length > 1) {
-         for (var m = 0; m < str.length; m++) {
-             var left = str[m];
-             var rest = str.slice(0, m) + str.slice(m + 1, str.length);
-             var preResult = fullpermutate(rest);
-             for (var i = 0; i < preResult.length; i++) {
-                 var tmp = left + preResult[i]
-                 // if(tmp.length === )
-                 if(tmp.length === 9 ) {
-                    if(checkAdd(tmp)) result.push(tmp);
-                 }
-                 else result.push(tmp);
-
-             }
-         }
-     } else if (str.length === 1) {
-         result.push(str);
-     }
-     return result;
- }
-
+ // function fullpermutate(str) {
+ //     var result = [];
+ //     if (str.length > 1) {
+ //         for (var m = 0; m < str.length; m++) {
+ //             var left = str[m];
+ //             var rest = str.slice(0, m) + str.slice(m + 1, str.length);
+ //             var preResult = fullpermutate(rest);
+ //             for (var i = 0; i < preResult.length; i++) {
+ //                 var tmp = left + preResult[i]
+ //                 // if(tmp.length === )
+ //                 if(tmp.length === 9 ) {
+ //                    if(checkAdd(tmp)) result.push(tmp);
+ //                 }
+ //                 else result.push(tmp);
+ //
+ //             }
+ //         }
+ //     } else if (str.length === 1) {
+ //         result.push(str);
+ //     }
+ //     return result;
+ // }
+ //
  // console.log(fullpermutate('123456789'))
+
+
 
 
  function fullpermutate1(str) {
@@ -55,7 +59,7 @@ function checkAdd(arr){
          for (var m = 0; m < str.length; m++) {
              var left = str[m];
              var rest = str.slice(0, m) + str.slice(m + 1, str.length);
-             var preResult = fullpermutate(rest);
+             var preResult = fullpermutate1(rest);
              for (var i = 0; i < preResult.length; i++) {
                  var tmp = left + preResult[i]
                  result.push(tmp);
