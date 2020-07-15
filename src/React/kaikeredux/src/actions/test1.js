c8ba1ef9e4e81465817a9395e90aef7f3027ac1b
peter <peterwangjiawei@gmail.com>
2020-07-14 21:19:12 -0500

    Jul 14 react 12345678910

--- src/React/kaikeredux/src/actions/test1.js
+++ src/React/kaikeredux/src/actions/test1.js
@@ -1,7 +1,7 @@
  // 1  2  3 4 5 6 7 8 9
- // 1 5 9
- // 6 7 2
-// 8 3 4
+ // 0 1 2
+ // 3 4 5
+//  6 7 8
 //
 function checkAdd(arr){
     for(let i = 0; i < 3; i++){
@@ -13,6 +13,13 @@ function checkAdd(arr){
                     parseInt(arr[6 + i])
         if(row !== 15 || col !== 15) return false
     }
+    let leftSlope = parseInt(arr[0]) +
+                    parseInt(arr[4]) +
+                    parseInt(arr[8])
+    let rightSlope = parseInt(arr[2]) +
+                        parseInt(arr[4]) +
+                        parseInt(arr[6])
+    if (leftSlope !== 15 || rightSlope !== 15) return false
     return true
 }
 
@@ -39,4 +46,26 @@ function checkAdd(arr){
      return result;
  }
 
- console.log(fullpermutate('123456789'))
+ // console.log(fullpermutate('123456789'))
+
+
+ function fullpermutate1(str) {
+     var result = [];
+     if (str.length > 1) {
+         for (var m = 0; m < str.length; m++) {
+             var left = str[m];
+             var rest = str.slice(0, m) + str.slice(m + 1, str.length);
+             var preResult = fullpermutate(rest);
+             for (var i = 0; i < preResult.length; i++) {
+                 var tmp = left + preResult[i]
+                 result.push(tmp);
+
+             }
+         }
+     } else if (str.length === 1) {
+         result.push(str);
+     }
+     return result;
+ }
+
+ console.log(fullpermutate1('123'))
