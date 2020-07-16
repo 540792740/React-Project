5d1ca630180bffa4114e5c5e2f2af00fbd1782b7
peter <peterwangjiawei@gmail.com>
2020-07-16 09:29:06 -0500

    Jul 16 react 1

--- src/React/kaikeredux/src/App.js
+++ src/React/kaikeredux/src/App.js
@@ -7,7 +7,6 @@ import ServiceFees from "./component/ServiceFees";
 import EstimatedTotal from "./component/EstimatedTotal";
 import ItemDetails from "./component/ItemDetails";
 import PromoCode from "./component/PromoCode";
-import LifeCycle from "./lifeCycle";
 
 class App extends Component {
     constructor(props) {
@@ -21,40 +20,39 @@ class App extends Component {
 
         }
     }
-    // componentDidMount() {
-    //     this.setState({
-    //         taxes:(this.state.total + this.state.PickupSavings) * 0.00875
-    //     },
-    //         function () {
-    //             this.setState({
-    //                 estimatedTotal:this.state.total + this.state.PickupSavings + this.state.taxes
-    //             })
-    //         })
-    // }
-    //
-    // giveDiscountHandler(){
-    //
-    // }
+    componentDidMount() {
+        this.setState({
+            taxes:(this.state.total + this.state.PickupSavings) * 0.00875
+        },
+            function () {
+                this.setState({
+                    estimatedTotal:this.state.total + this.state.PickupSavings + this.state.taxes
+                })
+            })
+    }
+
+    giveDiscountHandler(){
+
+    }
     render() {
         return (
             <div className="container">
                 {()=>this.control() }
-                {/*<Container className='purchase-card'>*/}
-                {/*    <Subtotal price={this.state.total.toFixed(2)}/>*/}
-                {/*    <PickupSsaving price={this.state.PickupSavings} />*/}
-                {/*    <ServiceFees taxes={this.state.taxes.toFixed(2)}/>*/}
-                {/*    <hr/>*/}
-                {/*    <EstimatedTotal price={this.state.estimatedTotal.toFixed(2)}/>*/}
-                {/*    <ItemDetails price={this.state.estimatedTotal.toFixed(2)}*/}
-                {/*                 PickupSavings={this.state.PickupSavings}*/}
-                {/*    />*/}
-                {/*    <hr/>*/}
-                {/*    <PromoCode giveDiscount={()=>this.giveDiscountHandler()}*/}
-                {/*               isDisabled={this.state.disablePromoButton}*/}
+                <Container className='purchase-card'>
+                    <Subtotal price={this.state.total.toFixed(2)}/>
+                    <PickupSsaving price={this.state.PickupSavings} />
+                    <ServiceFees taxes={this.state.taxes.toFixed(2)}/>
+                    <hr/>
+                    <EstimatedTotal price={this.state.estimatedTotal.toFixed(2)}/>
+                    <ItemDetails price={this.state.estimatedTotal.toFixed(2)}
+                                 PickupSavings={this.state.PickupSavings}
+                    />
+                    <hr/>
+                    <PromoCode giveDiscount={()=>this.giveDiscountHandler()}
+                               isDisabled={this.state.disablePromoButton}
 
-                {/*    />*/}
-                {/*</Container>*/}
-                <LifeCycle/>
+                    />
+                </Container>
             </div>
         );
     }
