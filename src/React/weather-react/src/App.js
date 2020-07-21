a3bc64c874837e99cb590b708c86b63073655a41
peter <peterwangjiawei@gmail.com>
2020-07-21 09:03:55 -0500

    Jul 21 react 1

--- src/React/weather-react/src/App.js
+++ src/React/weather-react/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 const api = {
   key : '586293560884d01267e4ed6b9001f137',
-  base: 'https://api.openweathermap/org/data/2.5/'
+  base: 'https://api.openweathermap.org/data/2.5/'
 }
 
 function App() {
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
+
+    const search = (event) =>{
+        if( event.key === 'Enter'){
+            fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+                .then(res => res.json())
+                .then(result => {
+                    setWeather(result);
+                    setQuery('');
+                    console.log(result)
+                });
+        }
+    }
+
     const dataBuilder = (d)=>{
         let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
         let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -23,6 +38,9 @@ function App() {
               <input type="text"
                      className='search-bar'
                      placeholder='Search...'
+                     onChange={e => setQuery(e.target.value)}
+                     value={query}
+                     onKeyPress={search}
               />
             </div>
 
