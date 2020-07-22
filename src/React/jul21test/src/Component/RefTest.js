5f92106371ba661d2726f6e45c4655b24488d59b
peter <peterwangjiawei@gmail.com>
2020-07-21 23:30:52 -0500

    Jul 21 react 12345

--- src/React/jul21test/src/Component/RefTest.js
+++ src/React/jul21test/src/Component/RefTest.js
@@ -4,12 +4,22 @@ class RefTest extends Component {
     constructor(props) {
         super(props);
         this.state = {}
+        this.myRef = React.createRef()
+    }
+    componentDidMount() {
+        this.myRef.current.focus()
     }
 
+    inputHandler(){
+        console.log(this.myRef.current.value)
+    }
     render() {
         return (
             <div>
-                
+                <input type="text"
+                       ref={this.myRef}
+                       onChange={(e)=>this.inputHandler(e)}
+                />
             </div>
         );
     }
