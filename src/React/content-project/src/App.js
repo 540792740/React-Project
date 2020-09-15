f7a0900b854d5d7081a5f498e9f217055ccb3f8a
peter <peterwangjiawei@gmail.com>
2020-09-15 17:46:54 -0500

    Sep 15 react 1122

---
+++ src/React/content-project/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Router from "./Component/Router";
+
+function App() {
+  return (
+    <div className="App">
+        Hello
+        <Router/>
+    </div>
+  );
+}
+
+export default App;
