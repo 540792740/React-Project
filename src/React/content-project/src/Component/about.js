c9ac2e3fd4eba2f47cd02b1747c92966e12899b1
peter <peterwangjiawei@gmail.com>
2020-09-15 21:32:03 -0500

    Sep 15 react 1123

--- src/React/content-project/src/Component/about.js
+++ src/React/content-project/src/Component/about.js
@@ -1,10 +1,8 @@
 import React, {useState} from 'react';
 import {useLocation, useHistory} from "react-router"
-
-function About(props) {
+function About() {
     const location = useLocation();
     const history = useHistory();
-    const [value, setValue] = useState();
     function goBackHandle() {
         history.goBack();
         console.log(history)
@@ -14,10 +12,6 @@ function About(props) {
             <div>About</div>
             <div>Location = {location.pathname}</div>
             <div>From={location.state.from}</div>
-            <input type="text"
-                   value={value}
-                   onChange={()=>setValue(value)}
-            />
             <button onClick={goBackHandle}>Go back</button>
         </React.Fragment>
     );
--- src/React/content-project/src/Component/user.js
+++ src/React/content-project/src/Component/user.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useParams} from 'react-router'
-
+import {Route, Switch} from "react-router-dom";
 function User() {
     const {firstName, lastName} = useParams();
     return (
